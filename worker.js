e946fc5694c6fc9af996c8257b4929836444b8a6
HDK Distribution Ltd <hdkdistributionltd@gmail.com>
2026-08-21 18:42:40 +0000

    Cache-bust HTML fetch for daily/dark/PWA

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 const GH = "https://raw.githubusercontent.com/4qmkjpnfbm-code/word-unscrambler/main/";
-const VER = "20260821l";
+const VER = "20260821m";
 const CANONICAL_HOST = "lettersunscrambler.com";
 const DICT = "https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt";
 const ROUTES = {
