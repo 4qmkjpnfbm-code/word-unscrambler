f6616e0595b9821752c31b530a70589a724124b9
HDK Distribution Ltd <hdkdistributionltd@gmail.com>
2026-08-21 17:13:20 +0000

    Add GA4 G-VR1EE3K51N with UK consent bar

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 const GH = "https://raw.githubusercontent.com/4qmkjpnfbm-code/word-unscrambler/main/";
-const VER = "20260821f";
+const VER = "20260821g";
 const CANONICAL_HOST = "lettersunscrambler.com";
 const DICT = "https://raw.githubusercontent.com/dolph/dictionary/master/enable1.txt";
 const ROUTES = {
